23d41d284b7c471a5f566f62106ac78ee25f2f60
slowchu <134579951+slowchu@users.noreply.github.com>
2026-07-20 04:26:03 -0700

    Delete examples directory

--- examples/consumer_adapter/scripts/example_beam_consumer/events.lua
+++
@@ -1,7 +0,0 @@
-local events = {}
-
--- Replace "ExampleAuthor" and "EasyBeams" with identifiers unique to your
--- mod. Global event names share one namespace with every other loaded mod.
-events.SHOW_FROST = "ExampleAuthor_EasyBeams_ShowFrost"
-
-return events
